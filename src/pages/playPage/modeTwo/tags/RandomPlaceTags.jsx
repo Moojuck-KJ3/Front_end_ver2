0dee9384af4793bd74ff136b3c247115d23c48e7
hnjog <43630972+hnjog@users.noreply.github.com>
2024-02-02 21:14:54 +0900

    mood2 관련 수식어 키워드 세팅 및 post 처리_세부 데이터 작업 x

--- src/pages/playPage/modeTwo/tags/RandomPlaceTags.jsx
+++ src/pages/playPage/modeTwo/tags/RandomPlaceTags.jsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from "react";
 import TagCard from "./TagCard";
 import { tagNames } from "./tagName";
 
+import { getMoodKeyword, postMoodKeywordButton } from "../../../../api";
+import { useSelector } from "react-redux";
+
 const RandomPlaceTags = () => {
   const [tags, setTags] = useState([]);
+  const roomId = useSelector((state) => state.roomId);
 
   // Function to get 12 random tags
   const getRandomTags = () => {
@@ -12,15 +16,38 @@ const RandomPlaceTags = () => {
   };
 
   useEffect(() => {
-    setTags(getRandomTags());
+    const getMoodKeywords = async (roomId) => {
+      const response = await getMoodKeyword(roomId);
+
+      if (response.error) {
+        console.log(response.exception);
+      } else {
+        // 가져온 수식어들을 태그처럼 활용할 예정
+        console.log(response.moodKeywords);
+        setTags(response.moodKeywords);
+      }
+    };
+
+    getMoodKeywords(roomId);
+
+    //setTags(getRandomTags());
   }, []);
+
+  const handleTagClick = (tag) => {
+    const data = {
+      keywordId: tag.id,
+      isDelete: false,
+    };
+
+    postMoodKeywordButton(roomId, data);
+  };
 
   return (
     <div className="w-3/4 flex border-1 shadow-md rounded-lg mx-10 bg-white justify-center ">
       <div className="w-full bg-gray-100 m-3 rounded-md shadow-md justify-center items-center flex ">
         <ul className="grid grid-cols-4 gap-3 p-10">
           {tags.map((tag, i) => (
-            <TagCard key={i} data={tag} />
+            <TagCard key={i} data={tag} handleClick={handleTagClick} />
           ))}
         </ul>
       </div>
