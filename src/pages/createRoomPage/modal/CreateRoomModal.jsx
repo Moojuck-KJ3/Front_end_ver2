a557051b91e137708bddd980b764d1f4e5643e55
hnjog <43630972+hnjog@users.noreply.github.com>
2024-02-03 13:19:30 +0900

    room create,join 관련 api 코드 추가_join은 아직 미사용

--- src/pages/createRoomPage/modal/CreateRoomModal.jsx
+++ src/pages/createRoomPage/modal/CreateRoomModal.jsx
@@ -3,8 +3,16 @@ import { Map, MapMarker } from "react-kakao-maps-sdk";
 import SearchIcon from "@mui/icons-material/Search";
 import { useNavigate } from "react-router-dom";
 
+import { createRoom } from "../../../api";
+import { useDispatch } from "react-redux";
+import { setRoomId } from "../../../redux_states/rooms";
+
 const API_KEY = "33d3165b1407ac3d92203219d067a088";
 const JS_KEY = "fa77f847dc1c042e320456f1f78748ad";
+
+const START_LAT = "37.498";
+const START_LNG = "127.028";
+const START_NAME = "강남역 2호선";
 
 const CreateRoomModal = ({ onSetting }) => {
   const navigate = useNavigate();
@@ -13,9 +21,32 @@ const CreateRoomModal = ({ onSetting }) => {
   const [map, setMap] = useState();
   const [searchQuery, setSearchQuery] = useState("");
 
+  // 만들때, back에 전달할 목표의 위도 경도
+  const [purPosePosition, setPurposePosition] = useState([
+    START_LAT,
+    START_LNG,
+  ]);
+  const [purPoseName, setPurposeName] = useState(START_NAME);
+  const dispatch = useDispatch();
+
   const handleCreateRoom = () => {
     // 방 생성 로직 여기
-    navigate("/waiting-friends");
+    const createRoomData = {
+      purposeName: purPoseName,
+      purposeAddress: purPosePosition,
+    };
+
+    const createRooms = async (createRoomData) => {
+      const response = await createRoom(createRoomData);
+      if (response.error) {
+        console.log(response.exception);
+      } else {
+        dispatch(setRoomId(response.roomId));
+        navigate("/waiting-friends");
+      }
+    };
+
+    createRooms(createRoomData);
   };
 
   const handleSearch = () => {
@@ -40,6 +71,9 @@ const CreateRoomModal = ({ onSetting }) => {
 
         setMarkers([marker]);
         setInfo(marker);
+
+        setPurposePosition([centerPosition.lat, centerPosition.lng]);
+        setPurposeName(marker.content);
 
         if (map) {
           map.setCenter(
