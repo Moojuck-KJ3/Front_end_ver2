142dd6ab8cd66360653d96cbdf3c9536fbc606ec
Horserio <a01049149446@gmail.com>
2024-02-04 14:36:32 +0900

    WebRTC 구현

--- src/pages/waitingPage/WaitingPage.jsx
+++ src/pages/waitingPage/WaitingPage.jsx
@@ -1,54 +1,21 @@
-import { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import WaitingFreindVideoContainer from "../createRoomPage/video/WaitingFreindVideoContainer";
 import CreateRoomPageFooter from "../createRoomPage/CreateRoomPageFooter";
 import {
-  createPeerConnection,
-  useAnswerProcessing,
+  useChatConnection,
   useLocalCameraStream,
-  useSendOfferSending,
-  useSendingAnswer,
+  usePeerConnection,
 } from "../../realtimeComunication/webRTCManager";
-import socket from "../../realtimeComunication/socket";
 
 const WaitingPage = () => {
   const navigator = useNavigate();
   const { roomId } = useParams();
   const { localStream } = useLocalCameraStream();
-  const [peerConnection, setPeerConnection] = useState(null);
-
-  useEffect(() => {
-    if (localStream) {
-      const initPeerConnection = async () => {
-        const pc = await createPeerConnection(localStream);
-        setPeerConnection(pc);
-      };
-      initPeerConnection();
-    }
-  }, [localStream]);
-
-  const { sendOffer } = useSendOfferSending(peerConnection, roomId);
-  const { handleConnectionOffer } = useSendingAnswer(peerConnection, roomId);
-  const { handleOfferAnswer } = useAnswerProcessing(peerConnection);
-
-  useEffect(() => {
-    console.log(peerConnection);
-    socket.on("answer", handleOfferAnswer);
-    socket.on("another-person-ready", sendOffer);
-    socket.on("send-connection-offer", handleConnectionOffer);
-
-    return () => {
-      socket.off("another-person-ready", sendOffer);
-      socket.off("send-connection-offer", handleConnectionOffer);
-      socket.off("answer", handleOfferAnswer);
-    };
-  }, [
-    peerConnection,
-    roomId,
-    sendOffer,
-    handleConnectionOffer,
-    handleOfferAnswer,
-  ]);
+  const { peerConnection, guestStream } = usePeerConnection(
+    localStream,
+    roomId
+  );
+  useChatConnection(peerConnection, roomId);
 
   const handleStartGame = () => {
     navigator(`/play-room/${roomId}`);
@@ -64,7 +31,7 @@ const WaitingPage = () => {
           {/* 비디오 */}
           <WaitingFreindVideoContainer
             localStream={localStream}
-            remoteStrem={localStream}
+            remoteStrem={guestStream}
           />
           {/* 버튼 */}
           <CreateRoomPageFooter onStart={handleStartGame} />
--- src/realtimeComunication/webRTCManager.js
+++ src/realtimeComunication/webRTCManager.js
@@ -1,10 +1,51 @@
 import socket from "./socket";
 import { useCallback, useEffect, useState } from "react";
 
-let peerConnection;
-const configuration = {
-  iceServers: [{ urls: "stun:stun2.1.google.com:19302" }],
-};
+export function useChatConnection(peerConnection, roomId) {
+  console.log("peerConnection", peerConnection);
+  const { sendOffer } = useSendOfferSending(peerConnection, roomId);
+  const { handleConnectionOffer } = useSendingAnswer(peerConnection, roomId);
+  const { handleOfferAnswer } = useAnswerProcessing(peerConnection);
+
+  const handleConnection = () => {
+    socket.on("connect", () => {
+      console.log("succesfully connected with socket.io server");
+      console.log(socket.id);
+    });
+  };
+
+  const handleReceiveCandidate = useCallback(
+    ({ candidate }) => {
+      peerConnection.addIceCandidate(candidate);
+    },
+    [peerConnection]
+  );
+
+  useEffect(() => {
+    if (!peerConnection) return;
+    socket.connect();
+    socket.on("connect", handleConnection);
+    socket.on("answer", handleOfferAnswer);
+    socket.on("another-person-ready", sendOffer);
+    socket.on("send-connection-offer", handleConnectionOffer);
+    socket.on("send_candidate", handleReceiveCandidate);
+
+    return () => {
+      socket.off("connect", handleConnection);
+      socket.off("answer", handleOfferAnswer);
+      socket.off("another-person-ready", sendOffer);
+      socket.off("send-connection-offer", handleConnectionOffer);
+      socket.off("send_candidate", handleReceiveCandidate);
+    };
+  }, [
+    peerConnection,
+    roomId,
+    handleOfferAnswer,
+    sendOffer,
+    handleConnectionOffer,
+    handleReceiveCandidate,
+  ]);
+}
 
 export function useLocalCameraStream() {
   const [localStream, setLocalStream] = useState(null);
@@ -22,15 +63,32 @@ export function useLocalCameraStream() {
   };
 }
 
-export const createPeerConnection = async (localStream) => {
-  peerConnection = new RTCPeerConnection(configuration);
+export function usePeerConnection(localStream, roomId) {
+  const [peerConnection, setPeerConnection] = useState(null);
+  const [guestStream, setGuestStream] = useState(null);
 
-  localStream.getTracks().forEach((track) => {
-    peerConnection.addTrack(track, localStream);
-  });
+  useEffect(() => {
+    if (!localStream) return;
 
-  return peerConnection;
-};
+    const pc = new RTCPeerConnection({
+      iceServers: [{ urls: "stun:stun2.1.google.com:19302" }],
+    });
+
+    pc.addEventListener("icecandidate", ({ candidate }) => {
+      socket.emit("send_candidate", { candidate, roomId });
+    });
+
+    localStream.getTracks().forEach((track) => pc.addTrack(track, localStream));
+
+    pc.addEventListener("track", ({ streams }) => {
+      setGuestStream(streams[0]);
+    });
+
+    setPeerConnection(pc);
+  }, [localStream, roomId]);
+
+  return { peerConnection, guestStream };
+}
 
 export function useSendOfferSending(peerConnection, roomId) {
   const sendOffer = useCallback(async () => {
@@ -52,6 +110,7 @@ export function useSendingAnswer(peerConnection, roomId) {
       const sessionDescription = new RTCSessionDescription(offer);
       await peerConnection.setRemoteDescription(sessionDescription);
       const answer = await peerConnection.createAnswer();
+      console.log(peerConnection, answer);
       await peerConnection.setLocalDescription(answer);
 
       socket.emit("answer", { answer, roomId });
