e72ad8511678c90997735168bba313a41419e9ae
Horserio <a01049149446@gmail.com>
2024-02-05 23:45:19 +0900

    #대기 상태 구현

--- src/pages/playPage/PlayRoomPage.jsx
+++ src/pages/playPage/PlayRoomPage.jsx
@@ -55,11 +55,11 @@ const PlayRoomPage = () => {
       if (data.roomReadyCount < 2) {
         setRoomReadyCount((prev) => prev + 1);
       } else if (data.roomReadyCount >= 2) {
-        console.log(roomReadyCount);
+        console.log(data.roomReadyCount);
         setRoomMode(data.newMode);
         setIsReady(false);
         setRoomReadyCount(0);
-        console.log(roomReadyCount);
+        console.log(data.roomReadyCount);
       }
     };
 
@@ -68,7 +68,7 @@ const PlayRoomPage = () => {
     return () => {
       socket.off("mode-change-response", handleModeChange);
     };
-  });
+  }, []);
 
   const handleSetReady = () => {
     console.log("handleSetReady is called");
