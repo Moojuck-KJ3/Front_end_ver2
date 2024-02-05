ba5e556275a54a04865fa5381bf2fb0b007942f5
hnjog <43630972+hnjog@users.noreply.github.com>
2024-02-05 23:25:33 +0900

    문제 해결용 코드

--- src/pages/playPage/PlayRoomPage.jsx
+++ src/pages/playPage/PlayRoomPage.jsx
@@ -110,22 +110,24 @@ const PlayRoomPage = () => {
         <GameArea>
           {/* 설명 모달 */}
           <div className="text-center">
-            {!showModal ? (
-              <ModeOneExpainModal
-                isShowModal={showModal}
-                onShow={setShowModal}
-              />
-            ) : (
-              <div className="w-full flex flex-col">
-                {/* 컨텐츠 */}
-                <div className="flex gap-5">
-                  {/* 좌측이 나 */}
-                  <VoiceRecoder isOwner={true} playerHand={playerHand} />
-                  {/* 우측이 다른 user */}
-                  <VoiceRecoder isOwner={false} playerHand={playerHand} />
-                </div>
+            <div className="w-full flex flex-col">
+              {/* 컨텐츠 */}
+              <div className="flex gap-5">
+                {/* 좌측이 나 */}
+                <VoiceRecoder
+                  isOwner={true}
+                  playerHand={playerHand}
+                  isCloseModal={showModal}
+                />
+                {/* 우측이 다른 user */}
+                <VoiceRecoder
+                  isOwner={false}
+                  playerHand={playerHand}
+                  isCloseModal={showModal}
+                />
               </div>
-            )}
+            </div>
+            <ModeOneExpainModal isShowModal={showModal} onShow={setShowModal} />
           </div>
 
           {/* 플레이어 핸드 */}
