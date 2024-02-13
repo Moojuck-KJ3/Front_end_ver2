9e4b75c758fedb49bd87b1c18818976a02d05075
hnjog <hnjogo@gmail.com>
2024-02-13 23:18:36 +0900

    조합 리스트에 데이터 삽입_mock 기준

--- src/pages/playPage/modeThree/card/ResultCard.jsx
+++ src/pages/playPage/modeThree/card/ResultCard.jsx
@@ -1,31 +1,32 @@
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
-const ResultCard = ({ title, imgUrl, id, likesCount }) => {
-  console.log();
+//title, imgUrl, id, likesCount
+const ResultCard = ({ place }) => {
+  console.log("place", place);
   return (
     <div
       className={`w-[160px] h-[180px] bg-transparent cursor-pointer group perspective relative`}
     >
       <div className="absolute z-10 -top-8 flex justify-center items-center w-10 h-8 bg-white rounded-full">
         <FavoriteIcon style={{ color: "red" }} />
-        {likesCount}
+        {place.likes}
       </div>
       <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
         <div className="absolute bg-white text-black  shadow-xl backface-hidden w-full h-full rounded-lg">
-          <img src={imgUrl} className="w-full h-full rounded-lg" />
+          <img src={place.thumbnailImg} className="w-full h-full rounded-lg" />
         </div>
         <div className="absolute my-rotate-y-180 backface-hidden w-full h-full bg-white shadow-xl overflow-hidden rounded-lg">
           <div className="text-center flex flex-col items-center h-full text-gray-800 px-2">
             <div className="my-4 gap-1 flex flex-col w-full p-3">
               <div className="flex justify-between">
                 <h3 className="text-sm font-medium text-gray-600">가게 이름</h3>
-                <p className=" text-black text-sm">{title}</p>
+                <p className=" text-black text-sm">{place.name}</p>
               </div>
               <div className="flex justify-between">
                 <h3 className="text-sm font-medium text-gray-600">
                   방문자 리뷰
                 </h3>
-                <p className="text-black text-sm ">1,402</p>
+                <p className="text-black text-sm ">{place.ratingCount}</p>
               </div>
               <div className="flex justify-between">
                 <h3 className="text-sm font-medium text-gray-600">
@@ -35,7 +36,7 @@ const ResultCard = ({ title, imgUrl, id, likesCount }) => {
               </div>
               <div className="flex justify-between">
                 <p className="text-gray-600 text-sm">리뷰수</p>
-                <p className="text-sm"> ⭐️4.5</p>
+                <p className="text-sm"> ⭐️{place.rating}</p>
               </div>
               <div className="flex justify-between">
                 <p className="text-gray-600 text-sm">영업중</p>
