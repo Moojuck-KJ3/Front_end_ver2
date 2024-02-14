40f1a3bb443f584c0c0f8bc7513199a992f11f1c
Horserio <a01049149446@gmail.com>
2024-02-14 21:10:10 +0900

    #playRoom 유저 비디어 성공

--- src/components/video/RightSideUserVideoContainer.jsx
+++ src/components/video/RightSideUserVideoContainer.jsx
@@ -5,7 +5,7 @@ const RightSideUserVideoContainer = ({ remoteStrem }) => {
   return (
     <div className=" flex flex-col w-1/5 min-w-[300px] h-full gap-4 ">
       <div className="flex  min-h-[300px] flex-col justify-center bg-white p-4 mx-2  rounded-lg shadow-2xl border-2 relative">
-        <VideoContainer mediaStream={remoteStrem[2]?.stream} />
+        <VideoContainer mediaStream={remoteStrem[1]?.stream} />
         {/* {showMic && (
           <button
             onClick={handleClickMic}
@@ -16,7 +16,7 @@ const RightSideUserVideoContainer = ({ remoteStrem }) => {
         )} */}
       </div>
       <div className="flex  min-h-[300px] flex-col justify-center bg-white p-4 mx-2  rounded-lg shadow-2xl border-2 ">
-        <VideoContainer mediaStream={remoteStrem[3]?.stream} />
+        <VideoContainer mediaStream={remoteStrem[4]?.stream} />
       </div>
       <div className="flex flex-col flex-grow  items-center justify-around bg-white px-10  mx-2 py-1 rounded-lg shadow-2xl border-4 border-red-500">
         <button className=" mx-auto w-full font-tenada text-4xl py-4 px-2  text-black bg-orange-400 hover:bg-orange-500 duration-150 ease-in-out hover:scale-105 transition-all rounded-full">
