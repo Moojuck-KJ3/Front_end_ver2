a21660962b9c228c2e94633329b5d55d6661cb66
Horserio <a01049149446@gmail.com>
2024-02-14 21:22:51 +0900

    #playerRoom 미디어 연결 완료

--- src/components/video/RightSideUserVideoContainer.jsx
+++ src/components/video/RightSideUserVideoContainer.jsx
@@ -16,7 +16,7 @@ const RightSideUserVideoContainer = ({ remoteStrem }) => {
         )} */}
       </div>
       <div className="flex  min-h-[300px] flex-col justify-center bg-white p-4 mx-2  rounded-lg shadow-2xl border-2 ">
-        <VideoContainer mediaStream={remoteStrem[4]?.stream} />
+        <VideoContainer mediaStream={remoteStrem[3]?.stream} />
       </div>
       <div className="flex flex-col flex-grow  items-center justify-around bg-white px-10  mx-2 py-1 rounded-lg shadow-2xl border-4 border-red-500">
         <button className=" mx-auto w-full font-tenada text-4xl py-4 px-2  text-black bg-orange-400 hover:bg-orange-500 duration-150 ease-in-out hover:scale-105 transition-all rounded-full">
